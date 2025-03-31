c62c795355f95b9518714ecf961e7e52b7dacfa2
martina panini <107107355+MartinaPanini@users.noreply.github.com>
2025-03-31 15:57:35 +0200

    dataset

---
+++ functions.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import math
+
+# This class computes and stores our vocab 
+# Word to ids and ids to word
+class Lang():
+    def __init__(self, corpus, special_tokens=[]):
+        self.word2id = self.get_vocab(corpus, special_tokens)
+        self.id2word = {v:k for k, v in self.word2id.items()}
+    def get_vocab(self, corpus, special_tokens=[]):
+        output = {}
+        i = 0 
+        for st in special_tokens:
+            output[st] = i
+            i += 1
+        for sentence in corpus:
+            for w in sentence.split():
+                if w not in output:
+                    output[w] = i
+                    i += 1
+        return output
+    
+class PennTreeBank (data.Dataset):
+    # Mandatory methods are __init__, __len__ and __getitem__
+    def __init__(self, corpus, lang):
+        self.source = []
+        self.target = []
+        
+        for sentence in corpus:
+            self.source.append(sentence.split()[0:-1]) # We get from the first token till the second-last token
+            self.target.append(sentence.split()[1:]) # We get from the second token till the last token
+            # See example in section 6.2
+        
+        self.source_ids = self.mapping_seq(self.source, lang)
+        self.target_ids = self.mapping_seq(self.target, lang)
+
+    def __len__(self):
+        return len(self.source)
+
+    def __getitem__(self, idx):
+        src= torch.LongTensor(self.source_ids[idx])
+        trg = torch.LongTensor(self.target_ids[idx])
+        sample = {'source': src, 'target': trg}
+        return sample
+    
+    # Auxiliary methods
+    
+    def mapping_seq(self, data, lang): # Map sequences of tokens to corresponding computed in Lang class
+        res = []
+        for seq in data:
+            tmp_seq = []
+            for x in seq:
+                if x in lang.word2id:
+                    tmp_seq.append(lang.word2id[x])
+                else:
+                    print('OOV found!')
+                    print('You have to deal with that') # PennTreeBank doesn't have OOV but "Trust is good, control is better!"
+                    break
+            res.append(tmp_seq)
+        return res
+
+def train_loop(data, optimizer, criterion, model, clip=5):
+    model.train()
+    loss_array = []
+    number_of_tokens = []
+    
+    for sample in data:
+        optimizer.zero_grad() # Zeroing the gradient
+        output = model(sample['source'])
+        loss = criterion(output, sample['target'])
+        loss_array.append(loss.item() * sample["number_tokens"])
+        number_of_tokens.append(sample["number_tokens"])
+        loss.backward() # Compute the gradient, deleting the computational graph
+        # clip the gradient to avoid exploding gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)  
+        optimizer.step() # Update the weights
+        
+    return sum(loss_array)/sum(number_of_tokens)
+
+def eval_loop(data, eval_criterion, model):
+    model.eval()
+    loss_to_return = []
+    loss_array = []
+    number_of_tokens = []
+    # softmax = nn.Softmax(dim=1) # Use Softmax if you need the actual probability
+    with torch.no_grad(): # It used to avoid the creation of computational graph
+        for sample in data:
+            output = model(sample['source'])
+            loss = eval_criterion(output, sample['target'])
+            loss_array.append(loss.item())
+            number_of_tokens.append(sample["number_tokens"])
+            
+    ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
+    loss_to_return = sum(loss_array) / sum(number_of_tokens)
+    return ppl, loss_to_return
+
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
+                elif 'weight_hh' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
+                elif 'bias' in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)
