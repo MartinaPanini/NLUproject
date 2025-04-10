ebfa168abb03af61ef51fb09d6d8d0433f1541cf
martinapanini <martina.panini@studenti@unitn.it>
2025-04-10 08:10:10 +0200

    changes

--- NLU/part_A/main.py
+++ NLU/part_A/main.py
@@ -13,6 +13,8 @@ import torch
 from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
+import copy
+import json
 
 if __name__ == "__main__":
 
@@ -21,6 +23,9 @@ if __name__ == "__main__":
     
     PAD_TOKEN = 0
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    tmp_train_raw = load_data(os.path.join('NLU/part_A/dataset/ATIS/train.json'))
+    test_raw = load_data(os.path.join('NLU/part_A/dataset/ATIS/test.json'))
 
     portion = 0.10
     intents = [x['intent'] for x in tmp_train_raw]
@@ -46,11 +51,6 @@ if __name__ == "__main__":
     dev_raw = X_dev
     y_test = [x['intent'] for x in test_raw]
 
-    # Dictionaries
-    w2id = {'pad': PAD_TOKEN, 'unk': 1}
-    slot2id = {'pad': PAD_TOKEN}
-    intent2id = {}
-
     words = sum([x['utterance'].split() for x in train_raw], [])
     corpus = train_raw + dev_raw + test_raw
     slots = set(sum([line['slots'].split() for line in corpus],[]))
@@ -68,31 +68,32 @@ if __name__ == "__main__":
     dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
     test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
 
-    # Training Setup
+    # ===== Training Params =====
     hid_size = 200
     emb_size = 300
-    lr = 0.01
+    lr = 0.0001
     clip = 5
     n_epochs = 200
     runs = 5
     patience = 5
+
     losses_train = []
     losses_dev = []
     sampled_epochs = []
     best_f1 = 0
+    all_slot_f1s = []
+    all_intent_accs = []
 
     out_slot = len(lang.slot2id)
     out_int = len(lang.intent2id)
     vocab_len = len(lang.word2id)
 
-    # Store results from all runs
-    all_slot_f1s = []
-    all_intent_accs = []
 
     # Train loop
     for x in tqdm(range(0, runs)):
         model = ModelIAS(hid_size, out_slot, out_int, emb_size, 
                         vocab_len, pad_index=PAD_TOKEN, isDropout=dropout, isBidirectional=bidirectional).to(device)
+        model.aplly(init_weights)
 
         optimizer = optim.Adam(model.parameters(), lr=lr)
         #optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
@@ -107,20 +108,22 @@ if __name__ == "__main__":
             if x % 5 == 0:
                 sampled_epochs.append(x)
                 losses_train.append(np.asarray(loss).mean())
-                results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
-                                                            criterion_intents, model, lang)
+                results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, criterion_intents, model, lang)
                 losses_dev.append(np.asarray(loss_dev).mean())
                 f1 = results_dev['total']['f']
 
                 if f1 > best_f1:
                     best_f1 = f1
+                    best_model = copy.deepcopy(model).to(device)
+                    patience = 3
                 else:
                     patience -= 1
                 if patience <= 0: # Early stopping with patient
                     break # Not nice but it keeps the code clean
+        
+        best_model.to(device)
 
-        results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
-                                                criterion_intents, model, lang)
+        results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, criterion_intents, model, lang)
         intent_acc.append(intent_test['accuracy'])
         slot_f1s.append(results_test['total']['f'])
     slot_f1s = np.asarray(slot_f1s)
@@ -140,24 +143,35 @@ if __name__ == "__main__":
     result_path = os.path.join("Results", model_name)
     os.makedirs(result_path, exist_ok=True)
 
-    results_txt = os.path.join(result_path, f"results_{model_name}.txt")
-    with open(results_txt, "w") as f:
-        f.write(f"{model_name}\n\n")
-        f.write(f"Slot F1: {round(np.mean(slot_f1s), 3)} ± {round(np.std(slot_f1s), 3)}\n")
-        f.write(f"Intent Accuracy: {round(np.mean(intent_acc), 3)} ± {round(np.std(intent_acc), 3)}\n")
-        f.write(f"Best dev F1: {round(best_f1, 3)}\n")
-
     # ==== Plot Losses ====
-    plt.figure()
-    plt.plot(sampled_epochs, losses_train, label='Train Loss')
-    plt.plot(sampled_epochs, losses_dev, label='Dev Loss')
+    plt.figure(figsize=(10, 6))
+    plt.plot(sampled_epochs, losses_train, label='Train Loss', marker='o')
+    plt.plot(sampled_epochs, losses_dev, label='Dev Loss', marker='s')
+    plt.xticks(sampled_epochs)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title(f'Training and Dev Loss {model_name}')
     plt.legend()
-    plt.grid()
+    plt.grid(True)
     plt.tight_layout()
     plt.savefig(os.path.join(result_path, "loss_plot.png"))
     plt.close()
 
+    # ===== Report =====
+    results_txt = os.path.join(result_path, f"results_{model_name}.txt")
+    with open(results_txt, "w") as file:
+        file.write(f"{model_name}\n\n")
+        file.write(f'epochs used: {sampled_epochs} \n')
+        file.write(f'number epochs: {n_epochs} \n')
+        file.write(f'lr: {lr} \n')
+        file.write(f'hidden_size: {hid_size} \n')
+        file.write(f'embedding_size: {emb_size} \n')
+        file.write(f'model: {model} \n')
+        file.write(f'optimizer: {optimizer} \n')
+        file.write(f'Slot F1: {round(np.mean(slot_f1s), 3)} ± {round(np.std(slot_f1s), 3)} \n')
+        file.write(f'Intent Accuracy: {round(np.mean(intent_acc), 3)} ± {round(np.std(intent_acc), 3)} \n')
+        file.write(f"Best dev F1: {round(best_f1, 3)}\n")
+        file.close()
 
+    
+   
